89da2b119cb7d554957c2ce38935b2365a82ce1b
rusgyn <rusgynmagsanay@gmail.com>
2024-06-15 17:06:46 -0400

    Refactor and implement the function to check if the file location is/not empty, and use readline to confirm.

--- fetcher.js
+++ fetcher.js
@@ -6,25 +6,62 @@
 
 const needle = require('needle');
 const fs = require('fs');
+const readline = require("readline");
+const removeAllListeners = require('process');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const argv = process.argv;
 const URL = argv[2]
 const destination = argv[3];
 
-needle.get(URL, (error, _response, body) => {
+// Error Handling. Script that checks if the location is empty/not empty.
+fs.readFile(destination, 'utf8', (error, data) => {
   if (error) {
-    return `There was an error: ${error}`;// Print the error if one occurred 
+    console.log(error);
+  } else if (data.length !== 0) { // Not empty
+
+    rl.question("Do you want to overwrite your file? Press Y for Yes or N for No ", (answer) => {
+      console.log(`You replied: ${answer}`);
+      if (answer === "Y") {
+        requestHandler(URL);
+      } else if (answer === "N") {
+        console.log("Thank you for trying. Consider different file location.");
+        rl.close();
+      } else {
+        console.log("Choose only Y or N")
+        rl.close();
+      }
+
+    rl.close();
+    });
+
+  } else if (data.length === 0) {
+    requestHandler(URL);
   }
 
-  responseHandler(body); // Passing the URL body content to callback function.
 });
+
+// Script that will make the HTTP request using needle
+const requestHandler = function(URL) {
+  needle.get(URL, (error, _response, body) => {
+    if (error) {
+      return `There was an error: ${error}`;// Print the error if one occurred 
+    }
+  
+    responseHandler(body); // Passing the URL body content to callback function.
+  });
+}
 
 // callback. Script that writes the data received to its destination.
 const responseHandler = function(response) {
   fs.writeFile(destination, response, error => {
     if (error) {
       return `There was an error writing the data: ${error}`;
-    } else {
+    } else  {
       dataSize(destination);
     }
   })
@@ -34,9 +71,13 @@ const responseHandler = function(response) {
 const dataSize = function(data) {
   fs.stat(data, (error, stats) => {
     if (error) {
-      return `There was an error getting the size of the data: ${error}`;
+      console.log(`There was an error getting the size of the data: ${error}`);
+      return;
     } else {
-      console.log(`Downloaded and saved ${stats.size} bytes to ${data}`)
+      console.log(`Downloaded and saved ${stats.size} bytes to ${data}`);
+      return;
     }
   });
 }
+
+rl.removeAllListeners();
